0a7650cb5c4a7b4500366678dcfebff510ceea2c
Payton Whinnery <paytoncaliw@gmail.com>
2024-03-27 09:31:22 -0500

    adding content to contact me page

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -16,28 +16,28 @@ export default function Footer() {
     <MDBFooter className='bg-dark text-center text-white fixed-bottom'>
       <MDBContainer className='p-4 pb-0'>
         <section className='mb-4'>
-          <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+          <MDBBtn outline color="light" floating className='m-1' href='https://www.facebook.com/profile.php?id=100091358232616' target="_blank" role='button'>
             <MDBIcon fab icon='facebook-f' />
           </MDBBtn>
 
-          <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+          {/* <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
             <MDBIcon fab icon='twitter' />
-          </MDBBtn>
+              </MDBBtn> */}
 
-          <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
-            <MDBIcon fab icon='google' />
+          <MDBBtn outline color="light" floating className='m-1' href='mailto:dressedtokillcomedy@gmail.com' role='button'>
+            <MDBIcon fab icon='google' /> 
           </MDBBtn>
-          <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+          <MDBBtn outline color="light" floating className='m-1' href='https://www.instagram.com/dressedtokillcomedy/' target="_blank" role='button'>
             <MDBIcon fab icon='instagram' />
           </MDBBtn>
 
-          <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
+          {/* <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
             <MDBIcon fab icon='linkedin-in' />
-          </MDBBtn>
-
+          </MDBBtn> */}
+{/* 
           <MDBBtn outline color="light" floating className='m-1' href='#!' role='button'>
             <MDBIcon fab icon='github' />
-          </MDBBtn>
+          </MDBBtn> */}
         </section>
       </MDBContainer>
 
